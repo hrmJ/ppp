1d5238ece9d90d7c32c434fed1507b0dbae3560d
Juho Härme <juho.harme@uta.fi>
2016-02-18 00:00:51 +0200

    first working version with tex examples

--- pandocpreprocessor.py
+++ pandocpreprocessor.py
@@ -14,30 +14,62 @@ class Document():
 
     def ConvertExamples(self):
         expat = re.compile(r'^ ?\(@(ee_[^\)]+)\)(.*)',re.MULTILINE)
-        endpat = re.compile(r'(^ ?\(@(ee_[^\)]+)\).*|^$)',re.MULTILINE)
         exmatch = expat.search(self.text)
+        examples = list()
         while exmatch:
+            ex = Example()
             rest = self.text[exmatch.end()+1:].splitlines()
+            length = 1
+            extext = exmatch.group(2)
+            exlabel = exmatch.group(1)
             for line in rest:
-                endmark = endpat.match(line)
+                endmark = expat.match(line)
                 if endmark:
-                    #Either the next example or an empty line
-                    self.text = self.text[:exmatch.start()] + self.WrapExe(exmatch.group(2),exmatch.group(1)) + self.text[exmatch.end()+1+endmark.start():]
-                    #self.text = "{left}{right}".format(left="1",right="2")
+                    #If a new example within the same paragraph
+                    ex.NewEx(exlabel,extext)
+                    extext = endmark.group(2)
+                    exlabel = endmark.group(1)
+                    length += len(line)
+                elif line:
+                    length += len(line)
+                    extext += " " + line
+
+                if not line:
+                    ex.NewEx(exlabel,extext)
+                    ex.End()
+                    self.text = self.text[:exmatch.start()] + ex.string + self.text[exmatch.end()+length+1:]
+                    examples.append(ex)
                     break
+
             exmatch = expat.search(self.text)
+
+        for example in examples:
+            self.text = example.AddReferences(self.text)
 
 
     def Finalize(self):
-        with open('output.{}'.format(self.doctype),'w') as f:
+        with open('output.md','w') as f:
             f.write(self.text)
 
+class Example():
 
-    def WrapExe(self, text, label):
-        if self.doctype == 'tex':
-            exestring =  "\n{}\n{}\n{}\n".format(r"\begin{exe}", r"\ex\label{" + label +"}" + text,  r"\end{exe}")
+    def __init__(self):
+        self.string = ""
+        self.labels = list()
 
-        return exestring
+    def NewEx(self,label, text):
+       self.string += "\n" + r"\ex.\label{" + label + "} " + text + "\n"
+       self.labels.append(label)
+
+    def End(self):
+        self.string += "\n\n"
+
+    def AddReferences(self,text):
+        for label in self.labels:
+            pat = re.compile("@" + label + r"\b")
+            text = pat.sub(r"\\ref{" + label + "}",text)
+
+        return text
 
 
 
