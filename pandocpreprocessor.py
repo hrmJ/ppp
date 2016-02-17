bd53151a21c19101ddbc1661c9c766d28cf78521
Juho Härme <juho.harme@uta.fi>
2016-02-17 21:18:48 +0200

    before iter

--- pandocpreprocessor.py
+++ pandocpreprocessor.py
@@ -6,14 +6,26 @@ import re
 class Document():
     """The text that is transformed"""
 
+    Document.Tex
+
     def __init__(self,fname,doctype):
         """Read text data etc """
         with open(fname,'r') as f:
-            self.text = f.read().splitlines()
+            self.lines = f.read().splitlines()
         self.doctype = doctype
 
     def ConvertExamples(self):
-        expat = re.compile(r'^\s?\(@(ee_[^\)]+)\)',re.MULTILINE)
+        expat = re.compile(r'^\s?\(@(ee_[^\)]+)\)')
+        exstart = None
+        exbegun = False
+        for idx, line in enumerate(self.lines):
+            exline = expat.match(line)
+            if exbegun and (exline or not line):
+
+            elif exline:
+                exlabel = exline.group(1)
+                exstart = exline.start()
+                exbegun=True
 
         #for exstart in expat.finditer(self.text):
         #    import ipdb; ipdb.set_trace()
