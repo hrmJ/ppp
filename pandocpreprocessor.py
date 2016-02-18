f1c2565ef13c19c64aa152fe1e9c6617e48a3241
Juho Härme <juho.harme@uta.fi>
2016-02-18 19:14:39 +0200

    definitions

--- pandocpreprocessor.py
+++ pandocpreprocessor.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
-
 import sys
 import re
+from bs4 import BeautifulSoup
 
 class Document():
     """The text that is transformed"""
 
+
+
     def __init__(self,fname,doctype):
         """Read text data etc """
-        with open(fname,'r') as f:
-            self.text = f.read()
+        if type(fname) == 'list':
+        else:
+            with open(fname,'r') as f:
+                self.text = f.read()
+                lines = self.text.splitlines()
+        #ycount = 0
+        #self.yaml = ""
+
+        #for idx, line in enumerate(lines):
+        #    self.yaml += line + "\n"
+        #    if line[0:3] == '---':
+        #        ycount += 1
+        #    if ycount == 2:
+        #        break
+
+        #self.text = "\n".join(lines[idx:])
+        #self.text = self.text[3:]
+        #self.text = self.text[3:8]
+
         self.doctype = doctype
+        if doctype=="tex":
+            self.preamp = r"""\documentclass[a4paper]{article}
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage[T2A]{fontenc}
+\usepackage[finnish]{babel}
+\usepackage{linguex} 
+\usepackage{amsthm}
+\newtheorem{maar}{Määritelmä}"""
 
     def ConvertExamples(self):
         expat = re.compile(r'^ ?\(@(ee_[^\)]+)\)(.*)',re.MULTILINE)
@@ -46,13 +74,32 @@ class Document():
         for example in examples:
             self.text = example.AddReferences(self.text)
 
+    def ConvertDefinitions(self):
+        """Divs with clas definition"""
+        #divpat = re.compile(r'.*<div .*definition.*')
+        #kkl
+        soup = BeautifulSoup(self.text)
+        defdivs = soup.findAll('div',{'class':'definition'})
+        for div in defdivs:
+            header = div.find('div',{'class','defheader'})
+            headertext = header.text
+            header.extract()
+            div.replaceWith(InsertDefinition(headertext,div.text))
+        self.text = str(soup)
+        self.text = self.text.replace('<html><body><p>','')
+        self.text = self.text.replace(r'</p></body></html>','')
+        self.text = self.text.replace('<html><body>','')
+        self.text = self.text.replace(r'</body></html>','')
+
 
     def Finalize(self):
         with open('output.md','w') as f:
+            #f.write("{}\n\n{}\n\n{}\n{}\n{}".format(self.yaml, self.preamp, r"\begin{document}" + "\n",self.text,r"\end{document}"))
             f.write(self.text)
 
-class Example():
 
+class Example():
+    """Linguistic example replacing @ee_"""
     def __init__(self):
         self.string = ""
         self.labels = list()
@@ -72,9 +119,16 @@ class Example():
         return text
 
 
+def InsertDefinition(htext, dtext):
+    """Insert a definition in tex"""
+    beginning = r"\begin{maar}" + "\n"
+    end = "\n" + r"\end{maar}"
+    #return beginning + htext + ":" + r"\\" + dtext + end
+    return beginning + dtext + end
 
 doc = Document(sys.argv[1],"tex")
 doc.ConvertExamples()
+doc.ConvertDefinitions()
 doc.Finalize()
 
 
